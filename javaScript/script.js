b6c4335ba85fb8edbe4fd1c102d07f7e7a3d42f8
Svyatoslav <Svyatoslav5j@gmail.com>
2023-02-19 23:12:40 +0300

    готово

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,21 +1,73 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
+const swiper = new Swiper('.review__swiper-block', {  
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+    loop: true
   });
+  
+  
+const swiperImg = new Swiper('.swiper-img', {
+  slidesPerView: 1,
+  centeredSlides: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop: true,
+  breakpoints: {
+    // when window width is >= 640px
+    700: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    }
+  }
+});
+
+
+// Находим форму в DOM
+let menuElement = document.querySelector('.popup-menu');// Воспользуйтесь методом querySelector()
+let menuOpenButton = document.querySelector('.header__menu-button');
+let popupCloseMenu = menuElement.querySelector('.popup-menu__close');
+
+let formElement = document.querySelector('.popap-form');// Воспользуйтесь методом querySelector()
+let formOpenButton = document.querySelector('.button_type_activ');
+let popupCloseform = formElement.querySelector('.popup-menu__close');
+
+function menuPopupOpen() {
+  menuElement.classList.add('popup-menu_activ');
+}
+function formPopupOpen() {
+  formElement.classList.add('popup-form_activ');
+}
+
+function menuPopupClose() {
+  menuElement.classList.remove('popup-menu_activ');
+}
+function formPopupClose() {
+  formElement.classList.remove('popup-form_activ');
+}
+
+menuOpenButton.addEventListener('click', menuPopupOpen)
+formOpenButton.addEventListener('click', formPopupOpen)
+
+popupCloseMenu.addEventListener('click',  menuPopupClose)
+popupCloseform.addEventListener('click',  formPopupClose)
+
+$(document).ready(function() {
+
+  //E-mail Ajax Send
+  $("popap__form").submit(function() {
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "/php/send.php",
+      data: th.serialize()
+    }).done(function() {
+      alert("Thank you!");
+    });
+    return false;
+  });
+
+});
